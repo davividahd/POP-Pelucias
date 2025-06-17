0f77bc3822c94642bec4e76a9d325532b9876eac
davividahd <daviricardosantos2007@gmail.com>
2025-06-17 16:44:34 -0300

    Update stylecard.js

--- src/componentes/card/stylecard.js
+++ src/componentes/card/stylecard.js
@@ -1,34 +1,27 @@
-// import { StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-// const styles = StyleSheet.create({
+ const styles = StyleSheet.create({
 
     
-//     imagecard: {
+    imagecard: {
 
-//         padding: 10, 
-//         marginVertical: 8,
-//         borderRadius: 8,
-//         shadowRadius: 4,
-//         width: 90, 
-//         height: 120,
-//         resizeMode: 'cover'
-//     },
+        padding: 10, 
+        marginVertical: 8,
+        borderRadius: 8,
+         width: 90, 
+        height: 120,
+         resizeMode: 'cover'
+    },
 
-//     textcard:{
-//         textAlign: 'center',
-//         fontSize: 15,
-//         color: "white",
-//         fontWeight: "bold",
-//         width: 100,
+   textcard:{
+         textAlign: 'center',
+         fontSize: 15,
+        color: "white",
+        fontWeight: "bold",
+        width: 100,
 
 
-//     },
+    },
+})
 
-//     textnota: { 
-//         textAlign: 'center',
-//         bordertop: 8,
-//         fontSize: 10,
-//         color: "yellow",
-//         fontWeight: "bold",
-//         width: 100,
-//     }};
+    export default styles;
